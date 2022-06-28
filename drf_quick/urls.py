fbc64c12a92ba523ec250f14d1f975bcc3221b46
Wellington Ideão <wellington.ideao@gmail.com>
2022-06-28 14:20:32 -0300

    Added a User and Group API Endpoint

--- drf_quick/urls.py
+++ drf_quick/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('quickstart.urls', namespace='quickstart')),
+    path('', include('quickstart.urls')),
+
 ]
--- quickstart/urls.py
+++ quickstart/urls.py
@@ -1,3 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from .views import UserViewSet, GroupViewSet
 
-app_name = 'quick_start'
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
+
